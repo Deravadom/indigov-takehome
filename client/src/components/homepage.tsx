295fd9dc45f851127e7a2dde891e5f424afd8a51
Michael Needleman <michael.needleman01@gmail.com>
2025-02-17 17:15:20 -0800

    Create simple data create and display page

--- client/src/components/homepage.tsx
+++ client/src/components/homepage.tsx
@@ -1,7 +1,30 @@
 import { useData, useMutation } from "../utils/hooks"
 
 const Homepage = () => {
-  return <></>
+  const { data, loading } = useData("contacts")
+
+  const [createContact] = useMutation<any>('contact')
+
+
+  const handler = () => {
+    createContact({
+      email: "foo@bar.com",
+      firstName: "foo2",
+      lastName: "bar"
+    })
+  }
+
+  return (
+    <div className="flex flex-column">
+      <span>Hello world!</span>
+      <button className="w4" onClick={handler}>Create Contact</button>
+      {data && (
+        <pre>
+          {JSON.stringify(data, null, 2)}
+        </pre>
+      )}
+    </div>
+  )
 }
 
 export default Homepage
