eb4e5be0d507a8809ec610e8a832ed88c0383ab3
Michael Needleman <michael.needleman01@gmail.com>
2025-02-17 17:45:27 -0800

    Add config and imports for export

--- api/config.ts
+++ api/config.ts
@@ -1,5 +1,7 @@
 import knex from "knex"
 import knexfile from "./knexfile"
+const { attachToStreamCSV } = require('knex-to-csv')
+attachToStreamCSV()
 
 const db = knex(knexfile.development)
 export default db;
